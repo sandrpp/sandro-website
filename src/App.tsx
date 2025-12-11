eca2db4bb2d24221a7581a4e92efcb7fa436d152
sandrpp <sandro.bohmer@tutanota.de>
2025-12-11 14:20:27 +0100

    removed "Hihihi" for testing

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}! Hihihi
+          count is {count}!
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
